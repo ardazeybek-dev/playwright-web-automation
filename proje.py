7d7130bf33014c9a971e8c0b728972428be5b82a
Pengu-tech <pengu4242.@gmail.com>
2026-06-04 17:44:07 +0300

    Add the final assignment code

---
+++ proje.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright
+
+def bot_projesi():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto("https://bergamamyo.ege.edu.tr/", wait_until="domcontentloaded")
+        
+        sayfa_basligi = page.title()
+        print(f"Başarıyla giriş yapılan site: {sayfa_basligi}")
+        
+        page.wait_for_timeout(3000)
+
+        page.screenshot(path="bergama_myo.png", full_page=True)
+
+        browser.close()
+
+if __name__ == "__main__":
+    bot_projesi()
